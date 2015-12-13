1b074498a4bb91e4640512cc004ce6a66c01c108
jorestma <jordi.estrada.m@gmail.com>
2015-12-13 20:47:40 +0100

    Commit

---
+++ tlab04/tlab04/build/CMakeFiles/tlab04.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tlab04.dir/main.o"
+  "tlab04.pdb"
+  "tlab04"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tlab04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
